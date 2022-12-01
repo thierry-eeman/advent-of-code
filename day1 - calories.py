d5b494c6023cf598ede8af5dbf79d725fec08e43
thierry-eeman <thierry.eeman@gmail.com>
2022-12-01 21:29:47 +0100

    Day 1 Complete A + B

---
+++ day1 - calories.py
@@ -0,0 +1,27 @@
+# Imports
+
+# Read Puzzle input
+with open("inputs/day1 - calories.txt", 'r') as file:
+    elf_calories_list = [{f'Elf {index+1}': [int(i) for i in group.split("\n")]} for index, group in enumerate(file.read().split("\n\n"))]
+    print(elf_calories_list)
+# Variables
+
+# Class
+
+# Independent functions
+
+
+# Main program
+def main():
+    totals = []
+    for elf in elf_calories_list:
+        calories = next(iter(elf.values()))
+        print(calories)
+        sum_calories = sum(calories)
+        totals.append(sum_calories)
+        top_three = sorted(totals)[-3:]
+    print(sum(top_three))
+
+
+if __name__ == "__main__":
+    main()
