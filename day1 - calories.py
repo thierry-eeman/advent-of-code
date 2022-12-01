6ffc0c289f949f63027ee15a4d3e336c5704c58f
thierry-eeman <thierry.eeman@gmail.com>
2022-12-01 21:43:35 +0100

    slight code cleanup day 1

--- day1 - calories.py
+++ day1 - calories.py
@@ -3,7 +3,6 @@
 # Read Puzzle input
 with open("inputs/day1 - calories.txt", 'r') as file:
     elf_calories_list = [{f'Elf {index+1}': [int(i) for i in group.split("\n")]} for index, group in enumerate(file.read().split("\n\n"))]
-    print(elf_calories_list)
 # Variables
 
 # Class
@@ -16,10 +15,9 @@ def main():
     totals = []
     for elf in elf_calories_list:
         calories = next(iter(elf.values()))
-        print(calories)
         sum_calories = sum(calories)
         totals.append(sum_calories)
-        top_three = sorted(totals)[-3:]
+    top_three = sorted(totals)[-3:]
     print(sum(top_three))
 
 
