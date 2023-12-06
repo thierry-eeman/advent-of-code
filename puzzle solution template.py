fa577573994887f75e0dbdfdbcb8231bcc130010
Eeman <ext17577@heraeus.com>
2023-12-06 16:31:27 +0100

    puzzle template added

---
+++ puzzle solution template.py
@@ -0,0 +1,30 @@
+# Imports
+
+
+# Path variables
+YEAR = int
+DAY = int
+PUZZLE_TITLE = "trebuchet"
+INPUT_PATH = f"{YEAR}/day {DAY} - {PUZZLE_TITLE}.txt"
+
+
+# Read Puzzle input
+with open(INPUT_PATH, 'r') as file:
+    pass
+
+# Variables
+
+
+# Class
+
+
+# Helper functions
+
+
+# Main function
+def main():
+    pass
+
+# Main program
+if __name__ == "__main__":
+    main()
