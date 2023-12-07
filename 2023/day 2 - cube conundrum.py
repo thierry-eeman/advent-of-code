1a31d2aa20c424c0b34d6c3facffeea1551f4f7e
thierry-eeman <thierry.eeman@gmail.com>
2023-12-07 23:42:51 +0100

    2023 day 2 cubeconundrum solved

--- 2023/day 2 - cube conundrum.py
+++ 2023/day 2 - cube conundrum.py
@@ -15,6 +15,7 @@ with open(INPUT_PATH, 'r') as file:
     # print(data)
 
 # Variables
+PART = 'B'
 MAX_RED = 12
 MAX_GREEN = 13
 MAX_BLUE = 14
@@ -27,31 +28,37 @@ class Game:
         self.green_cube = Cube()
         self.red_cube = Cube()
         self.blue_cube = Cube()
-        
+        self.cube_dict = {
+            'green': self.green_cube.values,
+            'blue': self.blue_cube.values,
+            'red': self.red_cube.values
+            }
+
         self.green_cube.color = 'green'
-        self.green_cube.amount = 0
         self.blue_cube.color = 'blue'
-        self.blue_cube.amount = 0
         self.red_cube.color = 'red'
-        self.red_cube.amount = 0
                         
     def print_game(self):
-        print(f"Game: {self.green_cube.amount} green, {self.blue_cube.amount} blue, {self.red_cube.amount} red")
+        print(f"Game {self.id}: {self.assert_possibility()}! {self.green_cube.values} green, {self.blue_cube.values} blue, {self.red_cube.values} red")
     
     def assert_possibility(self):
-        if self.blue_cube.amount > MAX_BLUE:
+        if max(self.blue_cube.values) > MAX_BLUE:
             return False
-        elif self.green_cube.amount > MAX_GREEN:
+        elif max(self.green_cube.values) > MAX_GREEN:
             return False
-        elif self.red_cube.amount > MAX_RED:
+        elif max(self.red_cube.values) > MAX_RED:
             return False
         else:
             return True
+    
+    def cube_power(self):
+        return max(self.green_cube.values) * max(self.red_cube.values) * max(self.blue_cube.values)
 
 class Cube:
     def __init__(self) -> None:
         self.color = str
         self.amount = int
+        self.values = []
     
 
 # Helper functions
@@ -61,28 +68,34 @@ class Cube:
 def main():
     possible_game_count = 0
     id_sum = 0
-    games = [re.split('; |, ', i.split(':')[1].strip()) for i in data]
-    print(games)
+    powers = []
+    games = [re.split('; ', i.split(':')[1].strip()) for i in data]
     for index, game in enumerate(games):
         new_game = Game()
         new_game.id = index + 1
-        for grab in game:
-            turn = grab.split(' ')
-            amount = int(turn[0])
-            color = turn[1]
-            match color:
-                case 'blue':
-                    new_game.blue_cube.amount += amount
-                case 'green':
-                    new_game.green_cube.amount += amount
-                case 'red':
-                    new_game.red_cube.amount += amount
+        for turn in game:
+            balls = turn.split(', ')
+            for idx, grab in enumerate(balls):
+                ball  = grab.split(' ')
+                amount = int(ball[0])
+                color = ball[1]
+                match color:
+                    case 'blue':
+                        new_game.blue_cube.values.append(amount)
+                    case 'green':
+                        new_game.green_cube.values.append(amount)
+                    case 'red':
+                        new_game.red_cube.values.append(amount)
         new_game.print_game()
+        powers.append(new_game.cube_power())
         if new_game.assert_possibility():
             possible_game_count += 1
             id_sum += new_game.id
+
     print(possible_game_count)
     print(id_sum)
+    print(powers)
+    print(sum(powers))
     
 # Main program
 if __name__ == "__main__":
