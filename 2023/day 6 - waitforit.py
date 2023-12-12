0bb28f71e78457ad2d54fb095e1e149a8127c717
Eeman <ext17577@heraeus.com>
2023-12-12 12:45:33 +0100

    2023 day 6 setup

---
+++ 2023/day 6 - waitforit.py
@@ -0,0 +1,30 @@
+# Imports
+
+
+# Path variables
+YEAR = 2023
+DAY = 6
+PUZZLE_TITLE = "waitforit"
+INPUT_PATH = f"./{YEAR}/inputs/day {DAY} - {PUZZLE_TITLE}.txt"
+
+
+# Read Puzzle input
+with open(INPUT_PATH, 'r') as file:
+    pass
+
+# Variables
+
+
+# Class
+
+
+# Helper functions
+
+
+# Main function
+def main():
+    pass
+
+# Main program
+if __name__ == "__main__":
+    main()
