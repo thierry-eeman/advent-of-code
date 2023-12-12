8a3061eb043d3c4584e3034fcc0f4efec3af171d
Eeman <ext17577@heraeus.com>
2023-12-12 13:24:38 +0100

    2023 day 12 hotsprings setup

---
+++ 2023/day 12 - hotsprings.py
@@ -0,0 +1,30 @@
+# Imports
+
+
+# Path variables
+YEAR = 2023
+DAY = "12"
+PUZZLE_TITLE = "hotsprings"
+INPUT_PATH = f"./{YEAR}/inputs/day {DAY} - {PUZZLE_TITLE}.txt"
+
+
+# Read Puzzle input
+with open(INPUT_PATH, 'r') as file:
+    pass
+
+# Variables
+
+
+# Class
+
+
+# Helper functions
+
+
+# Main function
+def main():
+    pass
+
+# Main program
+if __name__ == "__main__":
+    main()
