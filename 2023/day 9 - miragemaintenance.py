be8c373ccf27574cd19e1c2d964fad72ecf9a0e2
Eeman <ext17577@heraeus.com>
2023-12-12 13:02:42 +0100

    2023 day 9 setup

---
+++ 2023/day 9 - miragemaintenance.py
@@ -0,0 +1,30 @@
+# Imports
+
+
+# Path variables
+YEAR = 2023
+DAY = 9
+PUZZLE_TITLE = "miragemaintenance"
+INPUT_PATH = f"./{YEAR}/inputs/day {DAY} - {PUZZLE_TITLE}.txt"
+
+
+# Read Puzzle input
+with open(INPUT_PATH, 'r') as file:
+    pass
+
+# Variables
+
+
+# Class
+
+
+# Helper functions
+
+
+# Main function
+def main():
+    pass
+
+# Main program
+if __name__ == "__main__":
+    main()
