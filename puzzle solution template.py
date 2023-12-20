f3ba3c6aeccad920f0b3ac4a0b30bfa0300e4215
Eeman <ext17577@heraeus.com>
2023-12-20 11:21:22 +0100

    template dotenv changes

--- puzzle solution template.py
+++ puzzle solution template.py
@@ -1,11 +1,16 @@
 # Imports
-
+import os
+from dotenv import load_dotenv
+from pathlib import Path
 
 # Path variables
+dotenv_path = Path('./.env')
+load_dotenv(dotenv_path=dotenv_path)
 YEAR = int
 DAY = int
 PUZZLE_TITLE = str
 INPUT_PATH = f"./{YEAR}/inputs/day {DAY} - {PUZZLE_TITLE}.txt"
+PART = os.environ.get("PART", "A")
 
 
 # Read Puzzle input
