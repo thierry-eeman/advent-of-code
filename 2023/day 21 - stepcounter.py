8847f8e40adc43dde71f3a296a614ee36508dca2
Eeman <ext17577@heraeus.com>
2023-12-21 08:06:27 +0100

    2023 day 21 stepcounter setup

---
+++ 2023/day 21 - stepcounter.py
@@ -0,0 +1,35 @@
+# Imports
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+# Path variables
+dotenv_path = Path('./.env')
+load_dotenv(dotenv_path=dotenv_path)
+YEAR = 2023
+DAY = "21"
+PUZZLE_TITLE = 'stepcounter'
+INPUT_PATH = f"./{YEAR}/inputs/day {DAY} - {PUZZLE_TITLE}.txt"
+PART = os.environ.get("PART", "A")
+
+
+# Read Puzzle input
+with open(INPUT_PATH, 'r') as file:
+    pass
+
+# Variables
+
+
+# Class
+
+
+# Helper functions
+
+
+# Main function
+def main():
+    pass
+
+# Main program
+if __name__ == "__main__":
+    main()
