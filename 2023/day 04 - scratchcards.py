a53b8a0e4176230dd7e197f164ea8af514054489
Eeman <ext17577@heraeus.com>
2024-01-16 16:09:17 +0100

    2023 day 4 ongoing

--- 2023/day 04 - scratchcards.py
+++ 2023/day 04 - scratchcards.py
@@ -1,5 +1,5 @@
-# Imports
-
+# Import
+import re
 
 # Path variables
 YEAR = 2023
@@ -10,20 +10,32 @@ INPUT_PATH = f"./{YEAR}/inputs/day {DAY} - {PUZZLE_TITLE}.txt"
 
 # Read Puzzle input
 with open(INPUT_PATH, 'r') as file:
-    pass
+    data = [line.strip() for line in file.readlines()]
+    print(data)
 
 # Variables
-
+NUMBER_REGEX = r'\d+'
 
 # Class
-
+class ScratchCard:
+    def __init__(self) -> None:
+        self.instance = int
+        self.numbers = set()
+        self.winning = set()
 
 # Helper functions
-
+def winning_numbers(line):
+    winning_numbers = []
+    for number in re.findall(NUMBER_REGEX, line.split("|")[0]):
+        winning_numbers.append(int(number))
+    return winning_numbers[1:]
+    
 
 # Main function
 def main():
-    pass
+    for card in data:
+        winners = winning_numbers(card)
+        print(winners)
 
 # Main program
 if __name__ == "__main__":
