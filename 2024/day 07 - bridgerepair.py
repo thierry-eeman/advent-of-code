fd53106a0f68991fad296b59f4d6484ecde6b2ac
Thierry Eeman <thierry.eeman@gmail.com>
2024-12-16 09:26:22 +0100

    puzzle preparation day 14

--- 2024/day 07 - bridgerepair.py
+++ 2024/day 07 - bridgerepair.py
@@ -10,9 +10,9 @@ dotenv_path = Path('./.env')
 load_dotenv(dotenv_path=dotenv_path)
 YEAR = 2024
 DAY = '07'
-PUZZLE_TITLE = 'bridgerepair'
+PUZZLE_TITLE = 'debug'
 INPUT_PATH = f"./{YEAR}/inputs/day {DAY} - {PUZZLE_TITLE}.txt"
-PART = os.environ.get("PART", "A")
+PART = os.environ.get("PART", "B")
 
 
 # Read Puzzle input
@@ -22,11 +22,11 @@ with open(INPUT_PATH, 'r') as file:
     overlap_dict = {}
     for line in lines:
         key, values = line.split(":")
-        if int(key) in number_dict:
+        if int(key) in number_dict.keys():
             print(f"this number is already in there...: {key}")
             overlap_dict[int(key)] = list(map(int, values.split()))
         else: number_dict[int(key)] = list(map(int, values.split()))
-    # print(number_dict)
+    input = [number_dict, overlap_dict]
 
 # Variables
 correct_operations = defaultdict(list)
@@ -40,7 +40,10 @@ secound_round_sum = 0
 def collect_true_equations(key_outcome: int, value_list: list, operators=[]):
     if not operators:
         # All operator combo's for the full list of numbers
-        operators = list(product(["+", "*"], repeat=len(value_list)-1))
+        if len(value_list) > 1:
+            operators = list(product(["+", "*"], repeat=len(value_list)-1))
+        else:
+            operators = []
     
     # Calculate the default correct equations
     outcome, expression = calculate_result(key_outcome, value_list, operators)
@@ -62,11 +65,17 @@ def concatenate_numbers(number_list: list):
     return [list(result) for result in results]
 
 def calculate_result(target: int, number_list: list, operators: list):
-    for op_set in operators:
-        result = number_list[0]
-        expression = str(number_list[0])
+    result = number_list[0]
+    expression = str(number_list[0])
+    
+    if len(number_list) == 1 and target == result:
+        print(f"TRUE for {target} --> {expression}")
+        correct_operations[target].append(expression)
 
-        if len(number_list) > 1:
+    else:
+        for op_set in operators:
+            expression = str(number_list[0])
+            result = number_list[0]
             for number, operator in zip(number_list[1:], op_set):
                 expression += f" {operator} {number}"
                 if operator == "+":
@@ -74,63 +83,38 @@ def calculate_result(target: int, number_list: list, operators: list):
                 elif operator == "*":
                     result *= number
 
-        if result == target and expression not in correct_operations[target]:
-            print(f"TRUE for {target} --> {expression}")
-            correct_operations[target].append(expression)
+            if result == target and expression not in correct_operations[target]:
+                print(f"TRUE for {target} --> {expression}")
+                correct_operations[target].append(expression)
     
     return correct_operations[target], expression
 
 # Main function
 def main():
+    correct_operations_sum = 0
     global secound_round_sum
-    for number, number_list in number_dict.items():
-        collect_true_equations(number, number_list)
-        if PART == "B":
-            # All operator combo's when one number is concatenated in the list
-            if len(number_list) > 1:
-                operators_concat = list(product(["+", "*"], repeat=len(number_list)-2))
-            else:
-                operators_concat = []
-            
-            # Check if a correct equation for this number has already been found.
-            found_previous_run =  len(correct_operations[number])
-            
-            # create all alternative lists and iterate through those for correct equations.
-            concatenated_alternative_lists = concatenate_numbers(number_list)
-                               
-            for alt_number_list in concatenated_alternative_lists:
-                a, expression_1 = collect_true_equations(number, alt_number_list, operators_concat)
-                if len(correct_operations[number]) > found_previous_run:
-                    found_previous_run = len(correct_operations[number])
-                    secound_round_sum += number
-                    print(f"Found this result after concatenation: {number} - Original List {number_list} - {expression_1}")
+    for dict in input:
+        for number, number_list in dict.items():
+            collect_true_equations(number, number_list)
+            if PART == "B":       
+                # Check if a correct equation for this number has already been found.
+                found_previous_run =  len(correct_operations[number])
+                if found_previous_run == None:
+                    found_previous_run = 0
+                
+                # create all alternative lists and iterate through those for correct equations.
+                concatenated_alternative_lists = concatenate_numbers(number_list)
+                                
+                for alt_number_list in concatenated_alternative_lists:
+                    a, expression_1 = collect_true_equations(number, alt_number_list)
+                    if len(correct_operations[number]) > found_previous_run:
+                        found_previous_run = len(correct_operations[number])
+                        secound_round_sum += number
+                        print(f"Found this result after concatenation: {number} - Original List {number_list} - {expression_1}")
 
-    correct_operations_sum = sum(int(key) for key, value in correct_operations.items() if value)
+        correct_operations_sum += sum(int(key) for key, value in correct_operations.items() if value)
 
-    correct_operations.clear()
-    
-    for number, number_list in overlap_dict.items():
-        collect_true_equations(number, number_list)
-        if PART == "B":
-            # All operator combo's when one number is concatenated in the list
-            operators_concat = list(product(["+", "*"], repeat=len(number_list)-2))
-
-            found_previous_run =  len(correct_operations[number])
-            
-            # create all alternative lists
-            concatenated_alternative_lists = concatenate_numbers(number_list)
-            # print(f"{number_list} -> {concatenated_alternative_lists}")
-
-            for number_list in concatenated_alternative_lists:
-                a, expression_2 = collect_true_equations(number, number_list, operators_concat)
-                if len(correct_operations[number]) > found_previous_run:
-                    found_previous_run = len(correct_operations[number])
-                    secound_round_sum += number
-                    print(f"Found this result after concatenation: {number} - Original List {number_list} - {expression_2}")
-
-
-
-    correct_operations_sum += sum(int(key) for key, value in correct_operations.items() if value)
+        correct_operations.clear()
     
     print(f"The sum of all the true equations equals {correct_operations_sum}")
     print(f"The numbers found after concatenation add up to: {secound_round_sum}")
